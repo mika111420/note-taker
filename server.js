f3a598c7c079f6f2f78c5d312e802ba3f0051b9f
Mikayla <user@users-MacBook-Pro.local>
2023-06-23 15:23:08 -0400

    created post request to save file and return note

--- server.js
+++ server.js
@@ -18,15 +18,58 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,'/public/notes.html'));
-  });
-  
-  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
     const data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
     res.status(200).json(data);
-  
-  });
 
-  app.listen(PORT, () => {
+});
+
+app.post('/api/notes', (req, res) => {
+    res.json(`${req.method} request received to add a note`);
+
+    const {title,text} = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        });
+
+        ;
+
+
+        res.status(200)
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+
+
+
+});
+
+
+
+app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
-  })
+})
